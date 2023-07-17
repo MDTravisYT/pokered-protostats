6057b4005f5a96553652650be84b4e0fb29a4530
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-07-16 23:33:08 -0400

    encounter rates finished

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -12,5 +12,15 @@ CeruleanCaveB1FWildMons:
 	db	45, GOLBAT
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 15 ; encounter rate
+	db	39, POLIWAG
+	db	40, SHELLDER
+	db	40, HORSEA
+	db	39, HORSEA
+	db	39, SHELLDER
+	db	38, POLIWAG
+	db	40, POLIWAG
+	db	41, HORSEA
+	db	41, POLIWAG
+	db	41, SHELLDER
 	end_water_wildmons
